ee05e155fc50a1b1288dae62f002c02d598b9a5f
walterlv <xblero@gmail.com>
2018-07-11 09:15:42 +0800

    Add initial project files.

---
+++ src/dotnetCampus.SourceYard/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace dotnetCampus.SourceYard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello SourceYard!");
+        }
+    }
+}
