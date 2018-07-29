7976ae318ab440a09b3de2c9ff6ccf89df4b6135
lindexi <lindexi_gd@163.com>
2018-07-29 16:40:54 +0800

    添加注释

--- src/dotnetCampus.SourceYard/Utils/BuildProps.cs
+++ src/dotnetCampus.SourceYard/Utils/BuildProps.cs
@@ -5,39 +5,48 @@ using System.Xml.Serialization;
 
 namespace dotnetCampus.SourceYard.Utils
 {
-    [XmlType("PropertyGroup")]
     public class BuildProps
     {
+        /// <summary>
+        /// 版本号
+        /// </summary>
         public string Version { get; set; }
 
+        /// <summary>
+        /// 输出文件夹
+        /// </summary>
         public string PackageOutputPath { get; set; }
 
+        /// <summary>
+        /// 公司
+        /// </summary>
         public string Company { get; set; }
 
+        /// <summary>
+        /// 作者
+        /// </summary>
         public string Authors { get; set; }
 
+        /// <summary>
+        /// 仓库地址
+        /// </summary>
         public string RepositoryUrl { get; set; }
 
         public string RepositoryType { get; set; }
 
+        /// <summary>
+        /// 项目地址
+        /// </summary>
         public string PackageProjectUrl { get; set; }
 
+        /// <summary>
+        /// 版权
+        /// </summary>
         public string Copyright { get; set; }
 
+        /// <summary>
+        /// 描述
+        /// </summary>
         public string Description { get; set; }
-
-        public static BuildProps Parse(FileInfo file)
-        {
-            using (var stream = file.OpenText())
-            {
-                var load = XDocument.Load(stream);
-                var propertyGroup = load.Root.DescendantsAndSelf(XName.Get("PropertyGroup")).FirstOrDefault();
-
-                var xmlSerializer = new XmlSerializer(typeof(BuildProps));
-                var buildProps = (BuildProps) xmlSerializer.Deserialize(propertyGroup.CreateReader());
-
-                return buildProps;
-            }
-        }
     }
 }
