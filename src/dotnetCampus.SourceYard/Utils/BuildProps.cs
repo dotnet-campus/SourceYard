cce192b77d75e4d8e3ac615e5242bfd6b53a63b4
lindexi <lindexi_gd@163.com>
2018-09-29 10:50:17 +0800

    修复用户没有给作者安装

--- src/dotnetCampus.SourceYard/Utils/BuildProps.cs
+++ src/dotnetCampus.SourceYard/Utils/BuildProps.cs
@@ -1,45 +1,88 @@
-﻿using System.IO;
-using System.Linq;
-using System.Xml.Linq;
-using System.Xml.Serialization;
-
-namespace dotnetCampus.SourceYard.Utils
+﻿namespace dotnetCampus.SourceYard.Utils
 {
     public class BuildProps
     {
         /// <summary>
-        /// 公司
+        ///     公司
         /// </summary>
-        public string Company { get; set; }
+        public string Company
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_company))
+                {
+                    _company = "dotnet campus";
+                }
+
+                return _company;
+            }
+            set => _company = value;
+        }
 
         /// <summary>
-        /// 作者
+        ///     作者
         /// </summary>
-        public string Authors { get; set; }
+        public string Authors
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_authors))
+                {
+                    _authors = Owner;
+                }
+
+                return _authors;
+            }
+            set => _authors = value;
+        }
 
         /// <summary>
-        /// 仓库地址
+        ///     仓库地址
         /// </summary>
         public string RepositoryUrl { get; set; }
 
         public string RepositoryType { get; set; }
 
         /// <summary>
-        /// 项目地址
+        ///     项目地址
         /// </summary>
         public string PackageProjectUrl { get; set; }
 
         /// <summary>
-        /// 版权
+        ///     版权
         /// </summary>
         public string Copyright { get; set; }
 
         /// <summary>
-        /// 描述
+        ///     描述
         /// </summary>
-        public string Description { get; set; }
+        public string Description
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_description))
+                {
+                    _description = "This package is built by SourceYard";
+                }
 
-        public string Owner { get; set; }
+                return _description;
+            }
+            set => _description = value;
+        }
+
+        public string Owner
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_owner))
+                {
+                    _owner = "dotnet campus";
+                }
+
+                return _owner;
+            }
+            set => _owner = value;
+        }
 
         public string Title { get; set; }
 
@@ -51,5 +94,10 @@ namespace dotnetCampus.SourceYard.Utils
 
         public string PackageTags { get; set; }
 
+        private string _authors;
+        private string _company;
+
+        private string _description;
+        private string _owner;
     }
 }
