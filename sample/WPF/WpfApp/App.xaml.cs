472016c35dc0c596e6d563c3f81bfd1567d96b5e
lindexi <lindexi_gd@163.com>
2018-12-06 20:34:03 +0800

    添加 WPF 测试

---
+++ sample/WPF/WpfApp/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+        [STAThread]
+        private static void Main(string[] args)
+        {
+            var app = new App();
+            app.InitializeComponent();
+            app.Run();
+        }
+    }
+}
