90bb16fa26d1f553625e35e08d8665c750482eb1
lindexi <lindexi_gd@163.com>
2020-10-02 14:54:56 +0800

    加上安装代码

---
+++ src/dotnetCampus.SourceYard.Tool/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using dotnetCampus.Cli;
+using dotnetCampus.Cli.Standard;
+
+namespace dotnetCampus.SourceYard.Tool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CommandLine.Parse(args).AddStandardHandlers().AddHandler<InstallSourceOption>(InstallSource).Run();
+        }
+
+        private static void InstallSource(InstallSourceOption option)
+        {
+           // 下载对应的库
+        }
+    }
+
+    class InstallSourceOption
+    {
+        [Option(longName: nameof(Package))] 
+        public string Package { get; set; } = null!;
+
+        [Option(longName: nameof(Version))]
+        public string? Version { get; set; }
+    }
+}
