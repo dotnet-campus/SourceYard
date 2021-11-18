d5f56252149ebc8d370fbe9cbc3c286e0adb90b2
lindexi <lindexi_gd@163.com>
2021-11-18 15:29:41 +0800

    准备加上各个框架的写入
    这是逗比逻辑，准备干掉

---
+++ src/dotnetCampus.SourceYard/Cli/PackageInfoFileOptions.cs
@@ -0,0 +1,14 @@
+﻿using dotnetCampus.Cli;
+
+namespace dotnetCampus.SourceYard.Cli
+{
+    [Verb("Write")]
+    class PackageInfoFileOptions
+    {
+        [Option("MultiTargetingPackageInfoFile")]
+        public string MultiTargetingPackageInfoFile { set; get; } = null!;
+
+        [Option(longName: "TargetFramework")]
+        public string? TargetFramework { get; set; }
+    }
+}
