5ca9a7b396db1f1c5f5a5b3f0ab8c771db3c279d
lindexi <lindexi_gd@163.com>
2021-11-19 10:34:58 +0800

    集成测试

--- sample/App/AppUsingSource/Program.cs
+++ sample/App/AppUsingSource/Program.cs
@@ -10,6 +10,11 @@ namespace AppUsingSource
         {
             var money = new Money(12312);
             var v = (money, "123");
+            Console.WriteLine(v);
+
+#if NET45
+            var dependencyObject = new DependencyObject();
+#endif
 
             Console.WriteLine(money.ToCapital());
             Console.ReadLine();
